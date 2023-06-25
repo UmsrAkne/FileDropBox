108186b3e3727e664b1e1fddbd40c64d276536df
Akn <sibata486@gmail.com>
2023-06-26 00:14:12 +0900

    add / DestinationDirectory.Copy を実装

--- FileDropBox/Models/DestinationDirectory.cs
+++ FileDropBox/Models/DestinationDirectory.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
 namespace FileDropBox.Models
 {
     public class DestinationDirectory
@@ -6,6 +9,38 @@ namespace FileDropBox.Models
 
         public string Name { get; set; } = string.Empty;
 
-        public bool Exists { get; set; }
+        public bool Exists => Directory.Exists(DestPath);
+
+        /// <summary>
+        /// 引数に入力したパスが示すファイル、またはディレクトリを、このオブジェクトの DestPath の中にコピーします。
+        /// </summary>
+        /// <param name="targetFilePath">ファイル、ディレクトリのパス</param>
+        public void Copy([NotNull]string targetFilePath)
+        {
+            // 対象のパスのファイル、またはディレクトリがあるか確認する。
+            var existsFile = File.Exists(targetFilePath) || Directory.Exists(targetFilePath);
+            if (string.IsNullOrWhiteSpace(targetFilePath) || !existsFile)
+            {
+                return;
+            }
+
+            // ディレクトリの場合は直接 System.IO でのコピーが不可であるため、確認して処理を分岐させる。
+            var targetIsDirectory = Directory.Exists(targetFilePath);
+            var fileName = Path.GetFileName(targetFilePath);
+            var dest = $@"{DestPath}\{fileName}";
+
+            if (targetIsDirectory)
+            {
+                Microsoft.VisualBasic.FileIO.FileSystem.CopyDirectory(
+                    targetFilePath,
+                    dest,
+                    Microsoft.VisualBasic.FileIO.UIOption.AllDialogs,
+                    Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing);
+            }
+            else
+            {
+                File.Copy(targetFilePath, dest);
+            }
+        }
     }
 }
