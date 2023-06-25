826ded4027ce61c6fb00616acb1f7461e313af64
Akn <sibata486@gmail.com>
2023-06-25 23:51:51 +0900

    init /

---
+++ FileDropBox/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using FileDropBox.Views;
+using Prism.Ioc;
+
+namespace FileDropBox
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ FileDropBox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Mvvm;
+
+namespace FileDropBox.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title => "FileDropBox";
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
